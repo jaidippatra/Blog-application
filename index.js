57f8b52d25d18e351de5ccb33817663753127d1c
Jaidip Patra <jaidippatra@gmail.com>
2024-04-29 01:14:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import Quote from './quote.json' with { type: "json" };
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
-app.use(express.static("public"));
+app.use(express.static("Public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 
